d76fa39669db5c6e95b08bf152997f15e3cb2454
Egor <egormolchanov1401@gmail.com>
2022-07-24 16:57:34 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Egor Molchanov on 7/24/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
